ef8d38adfb5c37ae91be16fa9cd63ec4cc57a3e1
kotov <zloysergunya.lolz@gmail.com>
2021-01-06 14:50:58 +0700

    Настройка navigationBar

--- CatalogOfHouses/Screens/TabBar/TabBarController.swift
+++ CatalogOfHouses/Screens/TabBar/TabBarController.swift
@@ -15,6 +15,7 @@ class TabBarController: UITabBarController {
     }
     
     private func setupUI() {
+        setupNavigationBar()
         let mainVC = MainVC(nibName: "MainVC", bundle: nil)
         mainVC.tabBarItem.title = "Главная"
         mainVC.tabBarItem.image = UIImage.init(systemName: "square.grid.2x2")?.withTintColor(.white)
@@ -23,9 +24,31 @@ class TabBarController: UITabBarController {
         let aboutAppVC = AboutAppVC(nibName: "AboutAppVC", bundle: nil)
         aboutAppVC.tabBarItem.title = "Описание"
         aboutAppVC.tabBarItem.image = UIImage.init(systemName: "line.horizontal.3")?.withTintColor(.white)
-//        self.tabBar.barTintColor = UIColor(hex: "#707070")
-//        self.tabBar.alpha = 0.5
+        self.tabBar.barTintColor = UIColor(named: "blackMainColor")
+//        self.tabBar.alpha = 0.9
         viewControllers = [mainVC, aboutAppVC]
+    }
+    
+    private func setupNavigationBar() {
+        let leftButton = UIButton(type: .system)
+        leftButton.setImage(UIImage(systemName: "chevron.left")?.withTintColor(.white), for: .normal)
+        leftButton.tintColor = .white
+        leftButton.frame = CGRect.init(x: 0, y: 0, width: 30, height: 30)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+        
+        let middleImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 34, height: 38))
+        middleImageView.image = UIImage(named: "logo")
+        middleImageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = middleImageView
+        
+        let rightButton = UIButton(type: .system)
+        rightButton.setImage(UIImage(systemName: "ellipsis")?.withTintColor(.white), for: .normal)
+        rightButton.tintColor = .white
+        rightButton.frame = CGRect.init(x: 0, y: 0, width: 30, height: 30)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
+        
+        UINavigationBar.appearance().barTintColor = UIColor(named: "blackMainColor")
+        UINavigationBar.appearance().isTranslucent = false
     }
 
 }
